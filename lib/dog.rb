e6a463621cbebdedff2ef15feacb448e2f3d0d00
Dell Wilson <warren.wilsonjr89@gmail.com>
2020-05-12 20:03:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,18 @@
 class Dog  
-  def initialize(name, breed)
+  def initialize(name, breed = "Mutt")
     @name = name 
     @breed = breed 
   end
   
-  class Dog 
-attr_accessor :breed 
-def initialize 
-  @breed = "Mutt"
-end 
-end 
+#   class Dog 
+# attr_accessor :breed 
+# def initialize 
+#   @breed = "Mutt"
+# end 
+# end 
 
-d1 = Dog.new 
-d1.breed 
+# d1 = Dog.new 
+# d1.breed 
 
 
 #   def breed
